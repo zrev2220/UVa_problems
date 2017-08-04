859ddb8eac9327dfac023b8f89f265cc093a3cd8
Zac <zrev2220@gmail.com>
2017-08-04 14:22:53 -0400

    Update UVa12442.cpp - Seems faster, but still TLE

--- UVa12442.cpp
+++ UVa12442.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
+#include <bitset>
 using namespace std;
 // shortcuts for "common" data types in contests
 typedef long long ll;
@@ -15,29 +15,60 @@ typedef vector<int> vi;
 
 #define UNVISITED -1
 #define VISITED 1
-#define EXPLORED 2
 
 vi dfs_num;
-vector<vi> adjList;
-unordered_map<int, int> cycleLength; // unused right now
+bitset<500000> dfs_bit;
+vi adjList;
+vi cycleLength;
 
 int dfs(int u)
 {
+	if (cycleLength.at(u) != -1)
+		return cycleLength.at(u);
 	int size = 1;
-	dfs_num.at(u) = EXPLORED;
-	for (int j = 0; j < (int) adjList.at(u).size(); j++)
+	dfs_num.at(u) = VISITED;
+	dfs_bit.set(u);
+	int v = adjList.at(u);
+	while (dfs_num.at(v) == UNVISITED)
 	{
-		int v = adjList.at(u).at(j);
-		if (dfs_num.at(v) == UNVISITED)
+		size++;
+		dfs_num.at(v) = VISITED;
+		dfs_bit.set(v);
+		v = adjList.at(v);
+	}
+	// set cycleLength of all v's in cycle
+	int k = v;
+	if (k == u)
+	{
+		do
 		{
-			size += dfs(v);
+			cycleLength.at(v) = size;
+			v = adjList.at(v);
 		}
-		else if (dfs_num.at(v) == EXPLORED)
+		while (v != k);
+	}
+	else
+	{
+		int l = 1;
+		v = adjList.at(k);
+		do
 		{
-			// cycle check of some kind
+			l++;
+			v = adjList.at(v);
+		} while (v != k);
+		do
+		{
+			cycleLength.at(v) = l;
+			v = adjList.at(v);
+		}
+		while (v != k);
+		v = u;
+		for (int i = 0; v != k; i++)
+		{
+			cycleLength.at(v) = size - i;
+			v = adjList.at(v);
 		}
 	}
-	dfs_num.at(u) = VISITED;
 	return size;
 }
 
@@ -48,25 +79,30 @@ int main()
 	{
 		int n; scanf("%d", &n);
 		dfs_num.assign(n, UNVISITED);
-		adjList.assign(n, vi(0));
-		cycleLength.clear();
+		dfs_bit.reset();
+		adjList.assign(n, -1);
+		cycleLength.assign(n, -1);
 		ii bestMartian = make_pair(-1, -1);
 		// first = size; second = vertex
 		for (int i = 0; i < n; i++)
 		{
 			int u, v; scanf("%d %d", &u, &v);
-			adjList.at(u - 1).push_back(v - 1);
-			cycleLength[i] = 0;
+			adjList.at(u - 1) = v - 1;
 		}
 		for (int u = 0; u < n; u++)
 		{
-			int newSize = dfs(u);
-			if (newSize > bestMartian.first)
-				bestMartian = make_pair(newSize, u);
-			else if (newSize == bestMartian.first)
-				bestMartian.second = min(bestMartian.second, u);
-			dfs_num.assign(n, UNVISITED);
+			if (!dfs_bit.test(u))
+			{
+				int newSize = dfs(u);
+				if (newSize > bestMartian.first)
+					bestMartian = make_pair(newSize, u);
+				else if (newSize == bestMartian.first)
+					bestMartian.second = min(bestMartian.second, u);
+				dfs_num.assign(n, UNVISITED);
+			}
 		}
+		// for (int i = 0; i < n; i++)
+			// printf("## %d:%d\n", i, cycleLength.at(i));
 		printf("Case %d: %d\n", caseNo, bestMartian.second + 1);
 	}
 	return 0;
