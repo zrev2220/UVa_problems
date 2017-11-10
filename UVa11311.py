68fa837581a4f0449c6487b2f534d2269ca700bc
Zac <zrev2220@gmail.com>
2017-11-09 21:34:10 -0500

    Update UVa11311.py - AC

--- UVa11311.py
+++ UVa11311.py
@@ -2,5 +2,4 @@ import math
 
 for i in range(int(input())):
 	m,n,r,c = [int(x) for x in input().split(" ")]
-	print("{} ^ {} ^ {} ^ {} = {}".format(r * m, c * n, (r - n - 1) * m, (c - m - 1) * n, (r * m) ^ (c * n) ^ ((r - n - 1) * m) ^ ((c - m - 1) * n)))
-	print("Gretel" if ((r * m) ^ (c * n) ^ ((r - n - 1) * m) ^ ((c - m - 1) * n)) != 0 else "Hansel")
+	print("Gretel" if (r ^ c ^ (m - r - 1) ^ (n - c - 1) != 0) else "Hansel")
